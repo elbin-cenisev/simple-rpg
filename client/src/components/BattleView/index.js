c21d47fe16995526f740c4368c6427a5d6cbb490
Elbin Cenisev <elbincenisev@outlook.com>
2021-11-04 16:27:20 -0400

    Changed variables to make it easier to parse.

--- client/src/components/BattleView/index.js
+++ client/src/components/BattleView/index.js
@@ -16,8 +16,9 @@ function BattleView() {
   const player = state.player;  // The player character
   const enemy = state.enemy;     // The enemy the player is fighting
   
-  const [endGame, setGameover] = useState(false);
-  const [isAlive, setAliveness] = useState(true);
+  const [endGame, setGameover] = useState(false); // This will only toggle a turn after isPlayerAlive was set to false. Once it toggles, player is redirected to /gameover.
+  const [isPlayerAlive, setPlayerAliveness] = useState(true); // This toggles when the player's HP reaches 0 
+  const [isEnemyAlive, setEnemyAliveness] = useState(true); // This toggles when the enemy's HP reaches 0
   const [playerHP, setPlayerHP] = useState(player.maxHP) // Tracks enemy's current HP
   const [enemyHP, setEnemyHP] = useState(enemy.maxHP) // Tracks enemy's current HP
   const [commandsAvailable, setCommandAvailability] = useState(true); // Tracks whether user can use commands (does not mean it is their turn)
@@ -79,33 +80,42 @@ function BattleView() {
     console.log("use potion")
   }
 
-  // The player clicks the "Continue" button on the message-bar
+  // The player clicks the "Continue" button on the message-bar. This is where the bulk of the logic happens.
   function progressTurn() {
 
-    if(!isAlive) {
+    // If the player died in the last turn, the game is over
+    if (!isPlayerAlive) {
       setGameover(true);
     }
 
-    // Check whether someone died last turn
-    else if (checkAlive(enemyHP) == false) {
-      dispatch({
-        type: CHANGE_MESSAGE,
-        payload: `You have defeated the ${enemy.name}!!`,
-      })
-      setCommandAvailability(false);
-      endBattle(enemy);
+    // If the enemy died in the last turn, start tallying experience gains and such
+    else if (!isEnemyAlive) {
+      console.log("The enemy is dead");
+
+      // Calculate gold and experience gains
     }
 
-    else if (checkAlive(playerHP) == false) {
+    // Check if the player lost their HP during this turn. If they did, flip isPlayerAlive so that the next time the Continue is pressed, the game can end.
+    else if (checkForDeath(playerHP) == false) {
       dispatch({
         type: CHANGE_MESSAGE,
         payload: `You have been defeated!!!`,
       })
       setCommandAvailability(false);
-      setAliveness(false);
+      setPlayerAliveness(false);
     }
 
-    // Check whether it is the player's turn
+    // Check whether enemy's HP has reached 0. If it has, toggle the flag to make image disappear. Next time Continue is pressed, it'll calculate gold and exp gains.
+    else if (checkForDeath(enemyHP) == false) {
+      dispatch({
+        type: CHANGE_MESSAGE,
+        payload: `You have defeated the ${enemy.name}!!`,
+      })
+      setCommandAvailability(false);
+      setEnemyAliveness(false);
+    }
+
+    // If nobody died, check if it is the player's turn
     else if (playerTurn) {
       dispatch({
         type: CHANGE_MESSAGE,
@@ -113,16 +123,16 @@ function BattleView() {
       })
 
       setCommandAvailability(true); // If it is, let them choose a command
-      // }
     }
 
-    // Check whether it is the enemy's turn
+    // Check whether it is the enemy's turn. If it is, have the enemy attack by default.
     else if (enemyTurn) {
       enemyAttack()
     }
 
-    /* This handles the transition from playerTurn to enemyTurn. 
-    Note that there is no need to handle the transition from enemyTurn to playerTurn, so don't confuse this */
+    /* This ONLY handles the transition from playerTurn to enemyTurn (not vice-versa as there is no need) 
+    It might seem odd, but this in-between step allows for the message to be displayed informing the player
+    that the enemy is attacking*/
     else {
       dispatch({
         type: CHANGE_MESSAGE,
@@ -133,21 +143,17 @@ function BattleView() {
     }
   }
 
-  /* Pure function that returns product of randomly generated number between 1-10 
-  and either the player's or enemy's damage modifier */
+  /* Pure function that returns the product of randomly generated number between 1-10 
+  and the attacker's damage modifier */
   function calculateDamage(modifier) {
     return (Math.floor(Math.random() * (10 - 1 + 1)) + 1) * modifier;
   }
 
-  // Checks whether player or enemy is dead
-  function checkAlive(defenderHP) {
+  // Checks whether player or enemy lost all their health points
+  function checkForDeath(defenderHP) {
     if (defenderHP <= 0) {
       return false;
     } else { return true }
-  }
-
-  function endBattle(defeatedCombatant) {
-    setAliveness(false);
   }
 
   return (
@@ -165,7 +171,13 @@ function BattleView() {
 
       {/* The enemy-window just shows an image of the enemy you are fighting*/}
       <div id="enemy-window">
+
+        {/* If enemy is alive, show their picture */}
+        {isEnemyAlive ? (
         <img src={'./images/slime.png'} />
+        ) : (
+          console.log("The enemy is dead")
+        )}
       </div>
 
       {/* The command-bar shows all the commands a player can make durin their turn */}
@@ -183,7 +195,11 @@ function BattleView() {
         )}
         {endGame ? (
           <Redirect to="/gameover" />
-        ) : console.log("It's not over yet") }
+        ) : (console.log("It's not over yet"))}
+      </div>
+
+      <div id="health-bar">
+        {playerHP} / {player.maxHP}
       </div>
     </div>
   );
