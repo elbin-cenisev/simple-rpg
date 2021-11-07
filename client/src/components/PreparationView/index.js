588f8ef13dd2538f8e93bcc3b59a68a86f8927df
Elbin Cenisev <elbincenisev@outlook.com>
2021-11-07 13:04:02 -0500

    Just committing for transfer to other computer

---
+++ client/src/components/PreparationView/index.js
@@ -0,0 +1,11 @@
+import React, { } from 'react';
+
+function PreparationView() {
+  return (
+    <div id="preparationView">
+      <p>Test</p>
+    </div>
+  )
+}
+
+export default PreparationView;
--- client/src/pages/PreparationScreen.js
+++ client/src/pages/PreparationScreen.js
@@ -1,10 +1,12 @@
 import React, { } from 'react';
+import PreparationView from '../components/PreparationView'
 
 function PreparationScreen() {
   return (
     <>
       <div>
         <h1>Get prepared!</h1>
+        <PreparationView />
       </div>
     </>
   );
