0381247bb914ee7485810dee8ec8567a2ca40113
Elbin Cenisev <elbincenisev@outlook.com>
2021-11-07 19:21:49 -0500

    Message now display on entering the Inn

--- client/src/components/InnView/index.js
+++ client/src/components/InnView/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Redirect } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux';
-import { } from '../../utils/actions';
+import { CHANGE_MESSAGE } from '../../utils/actions';
 
 import './style.css'
 
@@ -10,19 +10,27 @@ function InnView() {
   // Since we manage a lot of states, just subscribe to the entire state
   let state = useSelector(state => state);
 
+  const dispatch = useDispatch();
+
   const player = state.player;  // The player character
   const message = state.message;  // The message that informs players of what is going on
 
-  const [storeVisit, setStoreVisit] = useState(false);
-  const [innVisit, setInnVisit] = useState(false);
+  const [cityVisit, setCityVisit] = useState(false);
 
-  function visitStore() {
-    setStoreVisit(true);
+  function returnToCity() {
+    setCityVisit(true);
   }
 
-  function visitInn() {
-    setInnVisit(true);
+  function restInn() {
+    
   }
+
+  useEffect(() => {
+    dispatch({
+      type: CHANGE_MESSAGE,
+      payload: `Would you like to rest for 50g?`,
+    })
+  }, []);
 
   return (
 
@@ -41,8 +49,8 @@ function InnView() {
       {/* The command-bar shows what the player can do */}
       <div id="command-bar">
         <div id="actions">
-          <button onClick={visitStore}>Go to Store</button>
-          <button onClick={visitInn}>Go to Rest</button>
+          <button onClick={returnToCity}>Leave Store</button>
+          <button onClick={restInn}>Rest</button>
         </div>
       </div>
 
@@ -56,11 +64,8 @@ function InnView() {
         {player.potions} Potions
       </div>
 
-      {storeVisit ? (
-        <Redirect to="/store" />
-      ) : (null)}
-      {innVisit ? (
-        <Redirect to="/inn" />
+      {cityVisit ? (
+        <Redirect to="/city" />
       ) : (null)}
 
     </div>
