8bff8caa1e8ed17a5a86952fd188fae48920afa1
Elbin Cenisev <elbincenisev@outlook.com>
2021-11-10 21:00:48 -0500

    Now displays user's characters in a list

--- client/src/components/CharacterSelectView/index.js
+++ client/src/components/CharacterSelectView/index.js
@@ -6,6 +6,9 @@ import Auth from '../../utils/auth';
 
 function CharacterSelectView() {
     const [userData, setUserData] = useState({});
+    const userDataLength = Object.keys(userData).length;
+    const charactersLength = Object.keys(userData.characters).length;
+
     const { loading, data } = useQuery(GET_USER);
 
     // On page load, get the user info (to get their characters)
@@ -17,8 +20,40 @@ function CharacterSelectView() {
 
     return (
         <>
-            <h1>Test</h1>
-
+            {userDataLength
+                ? <>
+                    <h1>Choose a character</h1>
+                    <ol>
+                        {charactersLength === 1
+                            ? <>
+                                <li>{userData.characters[0].characterName}</li>
+                            </>
+                            : <>
+                                <li>No character</li>
+                            </>
+                        }
+                        {charactersLength === 2
+                            ? <>
+                                <li>{userData.characters[1].characterName}</li>
+                            </>
+                            : <>
+                                <li>No character</li>
+                            </>
+                        }
+                        {charactersLength === 3
+                            ? <>
+                                <li>{userData.characters[2].characterName}</li>
+                            </>
+                            : <>
+                                <li>No character</li>
+                            </>
+                        }
+                    </ol>
+                </>
+                : <>
+                    <h1> LOADING </h1>
+                </>
+            }
         </>
     )
 }
