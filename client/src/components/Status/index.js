9665eca4cc49e8c831271f846c9d7df55bd6c88b
Elbin Cenisev <elbincenisev@outlook.com>
2021-11-12 13:49:32 -0500

    Status-bar now styled

--- client/src/components/Status/index.js
+++ client/src/components/Status/index.js
@@ -1,26 +1,73 @@
 import React, { } from 'react';
 import { useSelector } from 'react-redux';
-import { Progress } from 'semantic-ui-react';
+import { Progress, Table, Icon } from 'semantic-ui-react';
+
+import './style.css';
 
 function Status() {
     let player = useSelector(state => state.player);
     let healthPercentage = (player.currentHP * 100) / player.maxHP
 
     return (
-        <div id="statusBar">
+        <Table celled id="status-bar">
+            <Table.Header>
+                <Table.Row>
+                    <Table.HeaderCell  width={1} textAlign="left"><Icon name='male' /> Name</Table.HeaderCell>
+                    <Table.HeaderCell textAlign="left"><Icon name='heart' /> Health</Table.HeaderCell>
+                    <Table.HeaderCell width={2} textAlign="left"><Icon name='first aid' /> Potions</Table.HeaderCell>
+                    <Table.HeaderCell width={2} textAlign="left"><Icon name='sun' /> EXP</Table.HeaderCell>
+                    <Table.HeaderCell width={2} textAlign="left"><Icon name='rupee sign' />Gold</Table.HeaderCell>
+                </Table.Row>
+            </Table.Header>
 
-            {/* Shows player's current HP */}
-            <div id="health-bar">
-                <Progress percent={healthPercentage}>Health</Progress>
-                {player.currentHP} / {player.maxHP}
-            </div>
+            <Table.Body>
+                <Table.Row>
+                    <Table.Cell textAlign="left">{player.characterName}</Table.Cell>
+                    <Table.Cell textAlign="left"> <Progress percent={healthPercentage} success>{player.currentHP} / {player.maxHP}</Progress></Table.Cell>
+                    <Table.Cell textAlign="left">{player.potions}</Table.Cell>
+                    <Table.Cell textAlign="left">{player.exp}</Table.Cell>
+                    <Table.Cell textAlign="left">{player.gold}</Table.Cell>
+                </Table.Row>
+            </Table.Body>
+        </Table>
 
-            {/* Shows how many potions the player has in their posession */}
-            <div id="potion-bar">
-                {player.potions} Potions
-            </div>
+        /* <List horizontal relaxed='very' id="status-bar">
 
-        </div>
+            <List.Item id="player-name">
+                <List.Content>
+                    <List.Header><Icon name='male' /> Name</List.Header>
+                    {player.characterName}
+                </List.Content>
+            </List.Item>
+
+            <List.Item id="health-bar">
+                <List.Content>
+                    <List.Header><Icon name='heart' /> Health</List.Header>
+                    <Progress percent={healthPercentage} success>{player.currentHP} / {player.maxHP}</Progress>
+                </List.Content>
+            </List.Item>
+
+            <List.Item id="potion-bar">
+                <List.Content>
+                    <List.Header><Icon name='first aid' /> Potions</List.Header>
+                    {player.potions}
+                </List.Content>
+            </List.Item>
+
+            <List.Item>
+                <List.Content>
+                    <List.Header><Icon name='sun' /> EXP</List.Header>
+                    {player.exp}
+                </List.Content>
+            </List.Item>
+
+            <List.Item>
+                <List.Content>
+                    <List.Header><Icon name='rupee sign' />Gold</List.Header>
+                    {player.gold}
+                </List.Content>
+            </List.Item>
+        </List> */
     );
 };
 
